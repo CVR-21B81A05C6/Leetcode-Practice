b58690dbe18f353941f49121ca40d0b5b0bb0cf4
Ghandikota Venkata Sai Sashank <21b81a05c6@cvr.ac.in>
2025-09-07 13:09:11 +0530

    WiggleSort is added for greedy Solution

---
+++ greedy/wiggleSort.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void wiggleSort(vector<int>& nums) {
+        int n = nums.size();
+        auto mid = nums.begin() + n / 2;
+        nth_element(nums.begin(), mid, nums.end());
+        int median = *mid;
+
+        auto index = [n](int i) { return (1 + 2*i) % (n | 1); };
+
+        int i = 0, j = 0, k = n - 1;
+        while (j <= k) {
+            if (nums[index(j)] > median) {
+                swap(nums[index(i)], nums[index(j)]);
+                ++i; ++j;
+            } else if (nums[index(j)] < median) {
+                swap(nums[index(j)], nums[index(k)]);
+                --k;
+            } else {
+                ++j;
+            }
+        }
+    }
+};
